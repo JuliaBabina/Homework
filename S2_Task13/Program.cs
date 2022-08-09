89d7078d5bf1f7137c7aa308a8262f2549946517
Julia Babina <u.babina55@gmail.com>
2022-08-09 14:34:52 +0500

    add middle

---
+++ S2_Task13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.Clear();
+
+Console.WriteLine("Enter a number: ");
+
+string 
+
+// int num = Convert.ToInt32(Console.ReadLine());
+// int firstDigit = Math.Abs(num / 10);
+// int secondDigit = Math.Abs(firstDigit % 10);
+// int thirdDigit = Math.Abs(secondDigit % 10);
+
+// if (Math.Abs(num) / 100 > 0)
+// {
+//     Console.Write("Third digit: " + thirdDigit);
+// }
+// else
+// {
+//     Console.Write("This number does not have a third digit");
+// }
