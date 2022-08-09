c1313c9ba1e322f39b53581683af2c72d67fd273
Julia Babina <u.babina55@gmail.com>
2022-08-09 13:39:55 +0500

    add S2 Task 10

---
+++ S2_Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+
+Console.WriteLine("Enter a three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int firstDigit = Math.Abs(num / 10);
+int secondDigit = Math.Abs(firstDigit % 10);
+
+if (Math.Abs(num) / 100 > 0 && Math.Abs(num) / 100 < 10)
+{
+    Console.Write(secondDigit);
+}
+else
+{
+    Console.Write("It's not a three-digit number!");
+}
