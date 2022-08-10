4e6ee886f45773bcaa5b21de59276bb819c5b85d
Julia Babina <u.babina55@gmail.com>
2022-08-10 22:06:10 +0500

    add update to Task 13

--- S2_Task13/Program.cs
+++ S2_Task13/Program.cs
@@ -4,18 +4,42 @@
 // 32679 -> 6
 
 
+// Option №1
+
+// Console.Clear();
+
+// Console.WriteLine("Enter a number: ");
+// string numSeq = Convert.ToString(Console.ReadLine());
+// if (numSeq.Length > 2 )
+// {
+//     Console.Write("Third digit: " + numSeq[2]);
+// }
+
+// else
+// {
+//     Console.Write("This number does not have a third digit");
+// }
+
+// Option №2
+
 Console.Clear();
 
 Console.WriteLine("Enter a number: ");
-string numSeq = Convert.ToString(Console.ReadLine());
-if (numSeq.Length > 2 )
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (Math.Abs(num) / 100 > 0)
 {
-    Console.Write("Third digit: " + numSeq[2]);
+    if (Math.Abs(num) / 100 < 10) Console.Write("Third digit: " + Math.Abs(num) % 10);
+    if (Math.Abs(num) / 100 > 10 && Math.Abs(num) / 100 < 100) Console.Write("Third digit: " + Math.Abs(num) / 10 % 10);
+    if (Math.Abs(num) / 100 > 100 && Math.Abs(num) / 100 < 1000) Console.Write("Third digit: " + Math.Abs(num) / 100 % 10);
+    if (Math.Abs(num) / 100 > 1000 && Math.Abs(num) / 100 < 10000) Console.Write("Third digit: " + Math.Abs(num) / 1000 % 10);
+    if (Math.Abs(num) / 100 > 10000 && Math.Abs(num) / 100 < 100000) Console.Write("Third digit: " + Math.Abs(num) / 10000 % 10);
+    if (Math.Abs(num) / 100 > 100000 && Math.Abs(num) / 100 < 1000000) Console.Write("Third digit: " + Math.Abs(num) / 100000 % 10);
+    if (Math.Abs(num) / 100 > 1000000 && Math.Abs(num) / 100 < 10000000) Console.Write("Third digit: " + Math.Abs(num) / 1000000 % 10);
+    if (Math.Abs(num) / 100 > 10000000 && Math.Abs(num) / 100 < 100000000) Console.Write("Third digit: " + Math.Abs(num) / 10000000 % 10);
 }
 
 else
 {
     Console.Write("This number does not have a third digit");
 }
-
-
