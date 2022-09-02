03bc0264332c7906f360ca18124bf68a1b1d0a9f
Julia Babina <u.babina55@gmail.com>
2022-09-02 12:59:21 +0500

    add S9 2/3

---
+++ S9_All/Program.cs
@@ -0,0 +1,50 @@
+﻿
+// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+// Console.Clear();
+// Console.Write("Enter number M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter number N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"All natural numbers from {m} to {n}:");
+// Console.WriteLine(PrintNumbers(m, n));
+
+// string PrintNumbers(int start, int end)
+// {
+//     if (start == end) return start.ToString();
+//     return (start + " " + PrintNumbers(start + 1, end));
+// }
+
+
+
+//**********************************************************************************************************************
+
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Console.Clear();
+// Console.Write("Enter number M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter number N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"The sum of natural numbers from {m} to {n} is {SumNumbers(m, n)}");
+
+
+// int SumNumbers(int start, int end)
+// {
+//     if (start == end) return start;
+//     return (start + SumNumbers(start + 1, end));
+// }
+
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
